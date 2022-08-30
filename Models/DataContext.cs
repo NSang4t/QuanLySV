6104c3a37fd450994cfccb667fc88896b72cf5e3
hi <hi'@DESKTOP-877AFQH>
2022-08-30 15:40:34 +0700

    Add project files.

---
+++ Models/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cty.Models
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Roles> roless { get; set; }
+        public DbSet<UserModel> UserModels { get; set; }
+        public DbSet<LopHoc> LopHocs { get; set; }
+        public DbSet<Semester> Semesters { get; set; }
+        public DbSet<CT_Class> CT_Classs { get; set; }
+    }
+
+}
+
